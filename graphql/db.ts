e66b90b155edfb6188ef04883d2a3f159be8ac58
박성혜 <sperocometa@naver.com>
2021-07-12 18:06:02 +0900

    Update Scheme and Resolvers - Movie

--- graphql/db.ts
+++ graphql/db.ts
@@ -1,36 +1,47 @@
 export let movies = [
-    {
-        id : 1,
-        name : "Avengers",
-        score : 1
-    },
-    {
-        id : 2,
-        name : "The Lord of Ring",
-        score : 1
-    },
-    {
-        id : 3,
-        name : "Killing Time",
-        score : 1
-    },
-    {
-        id : 4,
-        name : "Spy",
-        score : 1
-    }
-]
+  {
+    id: 1,
+    name: 'Avengers',
+    score: 1,
+  },
+  {
+    id: 2,
+    name: 'The Lord of Ring',
+    score: 1,
+  },
+  {
+    id: 3,
+    name: 'Killing Time',
+    score: 1,
+  },
+  {
+    id: 4,
+    name: 'Spy',
+    score: 1,
+  },
+];
 
-export const getById = ( id : Number) =>{
-    const filteredMovies = movies.filter(movie => id === movie.id);
-    return filteredMovies[0];
-}
+export const getById = (id: Number) => {
+  const filteredMovies = movies.filter((movie) => id === movie.id);
+  return filteredMovies[0];
+};
 
-export const delteMovie = (id : Number)=>{
-    const cleanedMovies = movies.filter(movie => id !== movie.id);
-    if(movies.length > cleanedMovies.length){
-        movies = cleanedMovies;
-        return true;
-    }
-    return false;
-}
+export const deleteMovie = (id: Number) => {
+  const cleanedMovies = movies.filter((movie) => id !== movie.id);
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  }
+  return false;
+};
+export const addMovie = (name: string, score: number) => {
+  const newMovie = {
+    id: movies.length + 1,
+    name,
+    score,
+  };
+  console.log(newMovie);
+
+  movies.push(newMovie);
+  return newMovie;
+};
--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -1,10 +1,14 @@
-import { getById, movies } from "./db";
+import { getById, movies, addMovie, deleteMovie } from './db';
 
 const resolvers = {
-    Query : {
-        movies : () => movies,
-        movie : (_ : Object, { id }:any) => getById(id)
-    }
-}
+  Query: {
+    movies: () => movies,
+    movie: (_: Object, { id }: any) => getById(id),
+  },
+  Mutation: {
+    addMovie: (_: Object, { name, score }: any) => addMovie(name, score),
+    deleteMovie: (_: Object, { id }: any) => deleteMovie(id),
+  },
+};
 
 export default resolvers;
